46245df257fcf08cef8f869f5a7433f1fb55faaa
Reznikova Varia <31695448+RainbowLamb@users.noreply.github.com>
2018-12-02 15:01:15 +0300

    Create TestBall.java

---
+++ praktika1/TestBall.java
@@ -0,0 +1,14 @@
+public class TestBall
+{
+    public static void main(String[] args)
+    {
+        Ball ball1 = new Ball("White", 7);
+        Ball ball2 = new Ball("Red", 12);
+        Ball ball3 = new Ball("Blue");
+        ball3.setAge(3);
+        System.out.println();
+        ball1.intoBallColor();
+        ball2.intoBallColor();
+        ball3.intoBallColor();
+    }
+}
